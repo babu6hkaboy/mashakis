654a3d1e23f6a9ae30374b2f420c593f62fb5b70
babu6hkaboy <nikitasemukhin.ns@gmail.com>
2024-11-14 15:30:44 +0200

    V6

--- handlers/gpt_handler.py
+++ handlers/gpt_handler.py
@@ -1,22 +1,21 @@
 import openai
-from config import OPENAI_API_KEY, OPENAI_ASSISTANT_ID
-from handlers.telegram_notifier import send_telegram_notification_to_channel
+from config import OPENAI_API_KEY, OPENAI_ORGANIZATION, ASSISTANT_ID
 from utils.logger import logger
 
+# Устанавливаем API-ключ и организацию
+openai.organization = OPENAI_ORGANIZATION
 openai.api_key = OPENAI_API_KEY
 
 def generate_response(user_message):
     try:
-        response = openai.Assistant.create(
-            assistant=OPENAI_ASSISTANT_ID,
-            messages=[{"role": "user", "content": user_message}],
-            max_tokens=150,
-            temperature=0.7,
+        # Создаем запрос к ассистенту, используя assistant_id
+        response = openai.ChatCompletion.create(
+            assistant_id=ASSISTANT_ID,
+            messages=[
+                {"role": "user", "content": user_message}
+            ]
         )
-        bot_message = response.choices[0].message.content.strip()
-        return bot_message
+        return response.choices[0].message["content"].strip()
     except Exception as e:
-        logger.error(f"Error generating response: {e}")
-        # Отправляем уведомление в Telegram-канал в случае ошибки
-        send_telegram_notification_to_channel("Error", user_message)
+        logger.error(f"Error generating response from assistant: {e}")
         return "Извините, возникла техническая неполадка. Мы свяжемся с вами в ближайшее время."
